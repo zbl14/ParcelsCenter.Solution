993ad71d3eaac323a67fa48f64801cc9b8e9d4e9
zbl14 <ifthereisoneday@gmail.com>
2022-07-19 14:16:56 -0700

    add Parcel object, List<Parcel>, GetAll() for geting all parcels, ClearAll() for clearing the list of parcel

--- ParcelsCenter/Models/Parcel.cs
+++ ParcelsCenter/Models/Parcel.cs
@@ -2,6 +2,27 @@ namespace ParcelsCenter.Models
 {
   public class Parcel
   {
+    public string Dimensions { get; set; }
+    public int Weight { get; set; }
+    private static List<Parcel> _instances = new List<Parcel> {};
 
+    public Parcel(string dimensions, int weight)
+    {
+      Dimensions = dimensions;
+      Weight = weight;
+      _instances.Add(this);
+    }
+
+    public static List<Parcel> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    
   }
 }
